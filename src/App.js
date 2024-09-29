c25ce407c54ff7175b83c18ba58dda17b7ae0dcf
allanshin103 <shinyoung103@gmail.com>
2024-09-29 17:39:21 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer ={
+  'name': 'Hongkildong',
+  'birthday':'961222',
+  'gender':'male',
+  'job':'student'
+
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      
+      />
+    );
+  } 
 }
 
 export default App;
